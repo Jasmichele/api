a642df8b723df92c67f1c6293065f5fe4f6123ea
Jasmichele <jasmichele@yahoo.com>
2016-11-14 16:42:30 -0500

    mon class

---
+++ MyApi/MyApi/Controllers/FoodController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MyApi.Models;
+
+namespace MyApi.Controllers
+{
+    public class FoodController : ApiController
+    {
+        List<Food> food = new List<Food>
+        {
+            new Food { Id = 1, Name = "Pizza", Cuisine = "Italian", Price = 14.85M },
+            new Food { Id = 2, Name = "Taco", Cuisine = "Mexican", Price = 1.25M },
+            new Food { Id = 3, Name = "Fries", Cuisine = "French", Price = 2.30M },
+            new Food { Id = 4, Name = "Eggroll", Cuisine = "Chinese", Price = .85M },
+            new Food { Id = 5, Name = "Greens", Cuisine = "Soul", Price = 14.85M }
+        };
+
+        public IEnumerable<Food> GetAllFood()
+        {
+            return food;
+        }
+
+        public IHttpActionResult GetFood(int id)
+        {
+            var fod = food.SingleOrDefault(f => f.Id == id);
+
+            if(fod == null)
+            
+                return NotFound();
+
+            return Ok(fod);
+        }
+
+        public IHttpActionResult GetByCuisine(string cuis)
+        {
+            var tFood = food.SingleOrDefault(f => f.Cuisine.ToLower() == cuis.ToLower());
+
+            if (tFood == null)
+
+                return NotFound();
+
+            return Ok(tFood);
+
+        }
+    }
+}
---
+++ MyApi/MyApi/Models/Food.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace MyApi.Models
+{
+    public class Food
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Cuisine { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
